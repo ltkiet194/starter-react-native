4df1dd1fc46fa45d6c830b1815db16e88f39404f
Kiet Luong <ltkiet194@gmail.com>
2024-04-07 20:17:19 +0700

    finish First RN Project

--- App.tsx
+++ App.tsx
@@ -1,25 +1,42 @@
-import React from 'react';
-import {Text, View, TextInput,TouchableOpacity} from 'react-native';
+import React, { useState } from 'react';
+import {Alert,Text, View, TextInput, TouchableOpacity,ImageBackground} from 'react-native';
 
-export default function App() {
+
+
+export default function App(): JSX.Element {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const handleLogin = () => {
+    Alert.alert('Information', `Username: ${username}\nPassword: ${password}`);
+
+  };
   return (
-    <View className="p-8 bg-white rounded shadow-md w-96">
-      <View>
-        <View className="mb-4">
-          <TextInput  id="username"
-            placeholder="Enter your username"
-            className="block w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"></TextInput>
+    <ImageBackground source={{ uri: 'https://i.imgur.com/yxPtSCkl.jpg' }} >
+
+    <View className="h-full p-8 rounded shadow-md w-96">
+          <View className='p-4 mt-40 bg-white rounded-2xl'>
+            <View className="mb-4">
+              <Text className='font-bold text-center'>2024802010433</Text>
+            </View>
+            <View className="mb-4">
+              <TextInput  id="username"
+                placeholder="Enter your username"
+                onChangeText={text => setUsername(text)}
+                className="block w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"></TextInput>
+            </View>
+            <View className="mb-6">
+              <TextInput
+                id="password"
+                placeholder="Enter your password"
+                onChangeText={text => setPassword(text)}
+                className="block w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"></TextInput>
+            </View>
+          </View>
+          <TouchableOpacity className="px-6 py-4 mt-5 bg-blue-500 rounded-lg" onPress={handleLogin}>
+            <Text className='text-white'>Login</Text>
+          </TouchableOpacity>
         </View>
-        <View className="mb-6">
-          <TextInput
-            id="password"
-            placeholder="Enter your password"
-            className="block w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"></TextInput>
-        </View>
-      </View>
-      <TouchableOpacity className="px-6 py-4 bg-blue-500 rounded-lg">
-        <Text>Login</Text>
-      </TouchableOpacity>
-    </View>
+    </ImageBackground>
+
   );
 }
